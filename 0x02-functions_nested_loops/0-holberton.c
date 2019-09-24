2bfb0b81d3524aaff9bd6f71c444fd499aff6555
Simon Parra <simon.pahe@gmail.com>
2019-09-24 14:42:40 +0000

    program that prints Holberton, followed by a new line. The program should return 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,8 +1,14 @@
 #include <unistd.h>
 
-int _putchar(char c);
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
-int main()
+int _putchar(char c);
 {
 
 _putchar('H');
@@ -16,5 +22,5 @@ _putchar('o');
 _putchar('n');
 _putchar('\n');
 
-return(0);
+return (0);
 }
