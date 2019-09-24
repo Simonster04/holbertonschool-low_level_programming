c9972bdb27a5192faccc46e81c2e265f2c2b2dcc
Simon Parra <simon.pahe@gmail.com>
2019-09-24 14:51:57 +0000

    prints Holberton, followed by a new line. The program should return 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,14 +1,11 @@
-#include <unistd.h>
-
+#include "holberton.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - Entry point
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Always 0 (Success)
  */
 
-int _putchar(char c);
+int main(void)
 {
 
 _putchar('H');
