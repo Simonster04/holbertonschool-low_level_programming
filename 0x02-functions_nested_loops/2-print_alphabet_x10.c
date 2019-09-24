f6ed6648d12ad4d3e714ce92f7dc56db5f48211f
Simon Parra <simon.pahe@gmail.com>
2019-09-24 15:09:13 +0000

    prints 10 times the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,12 @@
 #include "holberton.h"
-#include <unistd.h>
+
+/**
+ * print_alphabet_x10 - writes the character c to stdout
+ *
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
 void print_alphabet_x10(void)
 
@@ -7,9 +14,9 @@ void print_alphabet_x10(void)
 
 char c;
 int n;
-	for(n ='0'; n <= '9'; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
-		for(c = 'a'; c <= 'z'; c++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
 		_putchar(c);
 		}
