32debced23eb1e5427f68e089b4c2fc22b4717c6
Simon Parra <simon.pahe@gmail.com>
2019-09-24 15:13:50 +0000

    checks for lowercase character. Returns 1 if c is lowercase, Returns 0 otherwise

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,19 +1,23 @@
 #include "holberton.h"
-#include <ctype.h>
-#include <unistd.h>
+
+/**
+ * _islower - checks for lowercase character
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, 0 is returned, and errno is set appropriately.
+ */
 
 int _islower(int c)
 
 {
-
-
 	if (c > 64 && c < 91)
 	{
-	return(0);
+	return (0);
 	}
 	else
 	{
-	return(1);
+	return (1);
 	}
 
 }
