b41187ce6d289fd624160806b9922bae6e4aad57
Simon Parra <simon.pahe@gmail.com>
2019-09-24 15:04:31 +0000

    prints the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,19 @@
-#include <unistd.h>
 #include "holberton.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ *
+ *
+ * Return: No return.
+ *
+ */
 
 void print_alphabet(void)
 
 {
 
 char c;
-
-	for(c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 	_putchar(c);
 	}
