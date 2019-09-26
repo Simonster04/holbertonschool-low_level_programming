2fdcb8d9e404463476b682993fdf810c69c94442
Simon Parra <simon.pahe@gmail.com>
2019-09-26 17:32:41 +0000

    multiplies two integers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,8 +2,8 @@
 
 /**
  * mul - multiplies two integers
- * @c: The character to print
- *
+ * @a: The character to print
+ * @b: The character to print
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
