3383b66da3298755f3f4cd673d0af32228585052
Simon Parra <simon.pahe@gmail.com>
2019-09-27 01:01:38 +0000

    prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,11 +1,10 @@
 #include "holberton.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * print_number - prints an integer.
+ * @n: The character to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: void.
  */
 
 void print_number(int n)
@@ -14,24 +13,30 @@ void print_number(int n)
 if (n >= 1000)
 {
 _putchar((n / 1000) + '0');
-_putchar((n / 100) + '0');
-_putchar((n / 10) + '0');
+_putchar(((n / 100) % 10) + '0');
+_putchar(((n / 10) % 10) + '0');
 _putchar((n % 10) + '0');
 }
-else if (n >= 100)
+else if (n >= 100 && n <= 999)
 	{
 	_putchar((n / 100) + '0');
-	_putchar((n / 10) + '0');
+	_putchar(((n / 10) % 10) + '0');
 	_putchar((n % 10) + '0');
 	}
-	else if (n >= 10)
+	else if (n >= 10 && n < 100)
 		{
 		_putchar((n / 10) + '0');
-        	_putchar((n % 10) + '0');
+		_putchar((n % 10) + '0');
 		}
-		else
-		{
-		_putchar((n / 10) + '0');
-        	_putchar((n % 10) + '0');
-		}
+		else if (n < 0)
+			{
+			n = n * -1;
+			_putchar('-');
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			}
+			else if (n >= 0 && n <= 9)
+			{
+			_putchar((n % 10) + '0');
+			}
 }
