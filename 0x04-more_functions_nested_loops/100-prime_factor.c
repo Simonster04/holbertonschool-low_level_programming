ffcbbbca1b2a6e77b04dd4f3e96ca59ad4d1f2c6
Simon Parra <simon.pahe@gmail.com>
2019-09-27 00:03:27 +0000

    finds and prints the largest prime factor of the number 61285247514

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the largest prime factor of the number 612852475143
+ *
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int main(void)
+{
+
+long int n = 612852475143;
+long int div = 2;
+long int max;
+
+while (n != 0)
+{
+	if ((n % div) != 0)
+	{
+	div = div + 1;
+	}
+	else
+	{
+	max = n;
+	n = n / div;
+		if (n == 1)
+		{
+		printf("Largest factor: %ld\n", max);
+		break;
+		}
+	}
+}
+return (0);
+}
