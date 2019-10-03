2e470636836d285d81069e727c31ccb53486cb8a
Simon Parra <simon.pahe@gmail.com>
2019-10-03 16:02:41 +0000

    concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strcat - returns the length of a string
+ * @src: source array
+ * @dest: destination array
+ * Return: 0.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+int i;
+int c = 0;
+
+while (dest[c] != '\0')
+{
+c += 1;
+}
+
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[c+i] = src[i];
+}
+return(dest);
+}
