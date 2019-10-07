42627d3788cc38356c6adc003484e6aa46c83af7
Simon Parra <simon.pahe@gmail.com>
2019-10-07 19:21:35 +0000

    gets the length of a prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: source array
+ * @accept: destination array
+ *
+ * Return: 0.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+
+int x, y;
+unsigned int cont = 0;
+
+for (x = 0; s[x] != ' ' || s[x] != ','; x++)
+{
+
+	for (y = 0; accept[y] != '\0'; y++)
+	{
+	if (accept[y] == s[x])
+	{
+	cont +=1;
+	}
+	}
+}
+
+return (cont);
+}
+
