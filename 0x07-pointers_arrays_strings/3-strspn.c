d7c0af1524ccb18aa70f234c76089f4a2bfeacaf
Simon Parra <simon.pahe@gmail.com>
2019-10-07 19:23:46 +0000

    gets the length of a prefix substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,14 +14,14 @@ unsigned int _strspn(char *s, char *accept)
 int x, y;
 unsigned int cont = 0;
 
-for (x = 0; s[x] != ' ' || s[x] != ','; x++)
+for (x = 0; s[x] != '\0' || s[x] != ' ' || s[x] != ','; x++)
 {
 
 	for (y = 0; accept[y] != '\0'; y++)
 	{
 	if (accept[y] == s[x])
 	{
-	cont +=1;
+	cont += 1;
 	}
 	}
 }
