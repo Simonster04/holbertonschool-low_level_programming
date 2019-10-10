0a51a983accc8c7394073ebd4d817e327f1f5eeb
vagrant <vagrant@precise32.(none)>
2019-10-10 00:40:37 +0000

    returns 1 if the input integer is a prime number, otherwise return 0

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+
+/**
+ * prime_calculation - returns 1 if the input is prime number, 0 otherwise
+ * @n: number
+ * @i: iterator
+ * Return: 1 success, 0 not success.
+ */
+
+int prime_calculation(int n, int i)
+{
+
+if ((n / i) > i)
+{
+return (prime_calculation(n, (i + 1)));
+}
+
+if ((n / i) == i)
+{
+	if ((i * i) == n)
+	{
+	return (0);
+	}
+	return (1);
+}
+
+return (1);
+
+}
+
+
+/**
+ * _is_prime_number - returns 1 if the input is prime number, 0 otherwise
+ * @n: number
+ * Return: 1 success, 0 not success.
+ */
+
+int is_prime_number(int n)
+{
+
+if (n <= 1)
+{
+return (0);
+}
+
+return (prime_calculation(n, 1));
+
+}
