7a1ccec7fafcbda8ada7722c295a2876ebbff257
vagrant <vagrant@precise32.(none)>
2019-10-10 03:00:59 +0000

    returns 1 if a string is a palindrome and 0 if no

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -7,4 +7,5 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt_calculation(int n, int i);
 int is_prime_number(int n);
-
+int is_palindrome(char *s);
+int palindrome_calculation(char *s, int i, int n);
