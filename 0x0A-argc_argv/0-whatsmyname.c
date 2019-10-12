a366cba6af3fbf788dc4e9f5272926402f90e5f6
vagrant <vagrant@precise32.(none)>
2019-10-12 22:42:05 +0000

    program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,16 +2,17 @@
 
 /**
  * main - Entry point
- *
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
  * Return: Always 0 (Success)
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 
-printf ("%s\n", argv[0]);
+printf("%s\n", argv[0]);
 argc++;
 
-return 0;
+return (0);
 
 }
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (int argc, char *argv[])
+{
+
+int i = 0;
+
+if (argc >= 0)
+{
+	printf("%d\n", i);
+}
+	else
+
+	{
+	printf("%s\n", argv[0]);
+	}
+
+return (0);
+}
