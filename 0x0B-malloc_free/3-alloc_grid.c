efcc29f1364147e2a69bbd1f8ccce773535d725a
Simon Parra <simon.pahe@gmail.com>
2019-10-15 23:08:16 +0000

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,5 +1,18 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  * str_concat - concatenates two strings
@@ -12,23 +25,36 @@
 int **alloc_grid(int width, int height)
 {
 
-int **x;
+int **x = NULL;
 int i;
 int j;
 
+if (height <= 0 || width <= 0)
+{
+	return (NULL);
+}
+
 x = (int **)malloc(height * sizeof(int*));
+
+if (x == NULL)
+{
+        return (NULL);
+}
 
 for (i = 0; i < height; i++)
 {
-	x[i] = (int*)malloc(columnas*sizeof(int));
+	x[i] = (int*)malloc(width * sizeof(int));
 }
 
-	for (i=0; i<filas; i++) 
-	{ 
-		printf("\n"); 
-		for (j=0; j<columnas; j++) 
-			printf("\t%d", x[i][j] ); 
-	} 
-	
-	return 0; 
-} 
+	for (i = 0; i < height; i++)
+	{
+		_putchar('\n');
+
+		for (j = 0; j < width; j++)
+		{
+			_putchar(0 + '0');
+		}
+	}
+	return 0;
+}
+
