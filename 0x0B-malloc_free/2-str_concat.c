391269d5594045b2f5f5058360fdbfeb8c065f80
Simon Parra <simon.pahe@gmail.com>
2019-10-15 21:27:05 +0000

    concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,36 +13,39 @@ char *str_concat(char *s1, char *s2)
 {
 
 char *fifi = NULL;
-unsigned int i;
-unsigned int c = 0;
+unsigned int i, c, d;
 
-while (s1[c] != '\0')
+if (s1 == '\0')
 {
-	c += 1;
+s1 = "";
+}
+if (s2 == '\0')
+{
+s2 = "";
 }
 
-unsigned int d = 0;
-
-while (s2[d] != '\0')
+for (c = 0; s1[c] != '\0'; c++)
 {
-	d += 1;
+}
+for (d = 0; s2[d] != '\0'; d++)
+{
 }
 
-fifi = malloc((c + d + 1)*sizeof(char));
+fifi = malloc((c + d + 1) * sizeof(char));
 
+if (fifi == NULL)
+{
+	return (NULL);
+}
 for (i = 0; s1[i] != '\0'; i++)
 {
 	fifi[i] = s1[i];
 }
-
 for (i = 0; s2[i] != '\0'; i++)
 {
 	fifi[c + i] = s2[i];
 }
-
 fifi[c + d] = '\0';
-
 return (fifi);
-
 
 }
