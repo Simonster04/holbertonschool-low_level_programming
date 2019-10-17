ccc4dc4b78a4252b53e533c924f0e7ed022e2f32
Simon Parra <simon.pahe@gmail.com>
2019-10-17 17:27:04 +0000

    function that allocates memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
- * create_array - creates an array of chars and initializes it with a char
- * @size: amount of chars
- * @c: specific char
+ * malloc_checked - allocates memory using malloc
+ * @b: size
  *
- * Return: &c
+ *
+ * Return: pointer to the allocated memory
  */
 
 void *malloc_checked(unsigned int b)
