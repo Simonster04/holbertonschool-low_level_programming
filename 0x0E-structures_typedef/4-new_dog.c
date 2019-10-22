d3eb62211379fc8a9a563b4b61e21a2003f299dc
Simonster04 <simon.pahe@gmail.com>
2019-10-22 03:49:58 +0000

    creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+	struct dog *new_dog = NULL;
+
+	new_dog = malloc(sizeof(struct dog));
+	if (new_dog == NULL)
+	{
+	return (NULL);
+	}
+
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+
+	return (new_dog);
+}
