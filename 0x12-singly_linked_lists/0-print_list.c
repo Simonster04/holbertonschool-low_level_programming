5105d67b68f2cb979177d83994f56533a7da453a
Simonster04 <simon.pahe@gmail.com>
2019-11-01 04:27:00 +0000

    prints all the elements of a list_t list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,18 +12,20 @@
 size_t print_list(const list_t *h)
 {
 	size_t cont = 0;
+	const list_t *tmp;
 
-	while (h)
+	tmp = h;
+	while (tmp)
 	{
-		if (!(h->str))
+		if (!(tmp->str))
 		{
 			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", tmp->len, tmp->str);
 		}
-		h = h->next;
+		tmp = tmp->next;
 		cont++;
 	}
 	return (cont);
