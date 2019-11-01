71989371b8e169bb8d41c73b1d509539bf9093a3
Simonster04 <simon.pahe@gmail.com>
2019-11-01 01:00:11 +0000

    function that adds a new node at the beginning of a list_t list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: char pointer
+ *
+ * Return: 0.
+ */
+
+int _strlen(const char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	return (c);
+}
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list.
+ * @head: head of the list
+ * @str: a string to be copied
+ * Return: address of new element.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+
+	list_t *new_node;
+
+	if (!str)
+	{
+		return (NULL);
+	}
+
+	new_node = malloc(sizeof(list_t));
+
+	if(!new_node)
+	{
+		return (NULL);
+	}
+
+	new_node->str = strdup(str);
+	new_node->len = _strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef _LIST_H_
 #define _LIST_H_
 
+#include <stdio.h>
 #include <stddef.h>
 
 /**
@@ -26,5 +27,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+int _strlen(const char *s);
 
 #endif
