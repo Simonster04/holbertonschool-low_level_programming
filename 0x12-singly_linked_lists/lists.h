c2670420cbee8a1085c11a872045091d94b06b45
Simonster04 <simon.pahe@gmail.com>
2019-11-02 14:46:45 +0000

    uploading library

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,7 +2,7 @@
 #define _LIST_H_
 
 #include <stdio.h>
-#include <stddef.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -16,17 +16,17 @@
 
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
-int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 int _strlen(const char *s);
+void first(void);
 
 #endif
