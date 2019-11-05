15581d88f9623e58fb036ce52147006f860a9f48
Simon Parra <simon.pahe@gmail.com>
2019-11-05 20:06:17 +0000

    returns the nth node of a list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,17 +14,21 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *tmp;
 	unsigned int i = 0;
 
-	tmp = head;
-	while (head && i < index)
+	if (head)
 	{
-		tmp = tmp->next;
-		i++;
+		tmp = head;
+		while (head && i < index)
+		{
+			tmp = tmp->next;
+			i++;
+		}
+		if (i != index)
+		{
+			return (NULL);
+		}
+
+		return (tmp);
 	}
 
-	if (i != index)
-	{
-		return (NULL);
-	}
-
-	return (tmp);
+	return (NULL);
 }
