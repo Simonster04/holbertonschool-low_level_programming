95bcc498aabe97a2811abdfa4f6696c9e61250da
Simonster04 <simon.pahe@gmail.com>
2019-11-08 03:38:43 +0000

    uploading endianness

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,7 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-int get_endianness(void);
 int _pow_recursion(int x, int y);
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -10,6 +9,7 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 
 #endif /*HOLBERTON_H*/
 
