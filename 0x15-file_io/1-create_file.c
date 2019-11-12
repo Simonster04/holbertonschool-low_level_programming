3cec378aa59ffe32c1d3e4a3a119b33d26626b2e
Simon Parra <simon.pahe@gmail.com>
2019-11-12 21:58:59 +0000

    creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,9 +18,14 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
-	if (!filename || !text_content)
+	if (!filename)
 	{
 		return (-1);
+	}
+
+	if (!text_content)
+	{
+		return (1);
 	}
 
 	if (fd == -1)
