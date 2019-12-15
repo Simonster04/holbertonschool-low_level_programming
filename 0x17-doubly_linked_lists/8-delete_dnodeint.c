3da5e0f26305a4aa09617abeba08377c8cd34cfd
Simonster04 <simon.pahe@gmail.com>
2019-12-15 22:09:58 +0000

    deletes the node at index index of a dlistint_t linked lis

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -38,7 +38,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		{
 			return (-1);
 		}
+
 		tmp->prev->next = tmp->next;
+
 		if (tmp->next)
 		{
 			tmp->next->prev = tmp->prev;
