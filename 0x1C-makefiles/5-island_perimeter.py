5a7e47e76626dabd37443484b54fe6d07923ab6c
Simonster04 <simon.pahe@gmail.com>
2020-02-21 22:07:59 -0500

    fixing lakes

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -20,5 +20,13 @@ def island_perimeter(grid):
                             perimeter += 1
                         if grid[list_num][uno - 1] == 0:
                             perimeter += 1
-
+        for list_num in range(len(grid) - 1):
+            if len(grid[list_num]) < 100 and list_num > 0:
+                for uno in range(len(grid[list_num]) - 1):
+                    if grid[list_num][uno] == 0 and uno > 0:
+                        if grid[list_num + 1][uno] == 1:
+                            if grid[list_num][uno + 1] == 1:
+                                if grid[list_num - 1][uno] == 1:
+                                    if grid[list_num][uno - 1] == 1:
+                                        perimeter -= 4
     return perimeter
