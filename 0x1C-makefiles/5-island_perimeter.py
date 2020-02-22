f4e2c161d7f918ee45356093967c21fddf787890
Simonster04 <simon.pahe@gmail.com>
2020-02-21 21:31:48 -0500

    addingtask 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+containd island_perimeter function
+"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid"""
+    perimeter = 0
+    for list_num in range(len(grid)):
+        for uno in range(len(grid[list_num])):
+            if grid[list_num][uno] == 1:
+                if grid[list_num + 1][uno] == 0:
+                    perimeter += 1
+                if grid[list_num][uno + 1] == 0:
+                    perimeter += 1
+                if grid[list_num - 1][uno] == 0:
+                    perimeter += 1
+                if grid[list_num][uno - 1] == 0:
+                    perimeter += 1
+
+    return perimeter
