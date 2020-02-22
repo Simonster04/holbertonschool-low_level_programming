58546032f4fdc55d1ebe18a534c86498e790bda8
Simonster04 <simon.pahe@gmail.com>
2020-02-22 10:03:25 -0500

    going back

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,22 +8,20 @@ def island_perimeter(grid):
     """returns the perimeter of the island described in grid"""
     perimeter = 0
     if len(grid) < 100:
-        for list_num, lista in enumerate(grid):
+        for list_num in range(len(grid) - 1):
             if len(grid[list_num]) < 100:
-                for uno, valor in enumerate(grid[list_num]):
-                    print(valor)
-                    if grid[list_num][uno] == 1:
-                        perimeter += 4
-                        if list_num < (len(grid) - 1):
-                            if grid[list_num + 1][uno] == 1:
-                                perimeter -= 1
-                        if uno < (len(lista) - 1):
-                            if grid[list_num][uno + 1] == 1:
-                                perimeter -= 1
-                        if not grid[list_num - 1] or grid[list_num - 1][uno] == 1:
-                            perimeter -= 1
-                        if not grid[list_num][uno - 1] or grid[list_num][uno - 1] == 1:
-                            perimeter -= 1
+                for uno in range(len(grid[list_num]) - 1):
+                    if grid[list_num][uno] != 0:
+                        if list_num < len(grid) - 1:
+                            if not grid[list_num + 1] or grid[list_num + 1][uno] != 1:
+                                perimeter += 1
+                        if uno < len(grid[list_num]):
+                            if not grid[list_num][uno + 1] or grid[list_num][uno + 1] != 1:
+                                perimeter += 1
+                        if grid[list_num - 1][uno] != 1:
+                            perimeter += 1
+                        if grid[list_num][uno - 1] != 1:
+                            perimeter += 1
 
         for list_num in range(len(grid) - 1):
             if len(grid[list_num]) < 100 and list_num > 0:
