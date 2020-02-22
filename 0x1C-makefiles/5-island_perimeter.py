4826e3fbb213198e13d26b9e4229e919bdfdba3c
Simonster04 <simon.pahe@gmail.com>
2020-02-21 21:58:39 -0500

    fixing conditions

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,16 +9,16 @@ def island_perimeter(grid):
     perimeter = 0
     if len(grid) < 100:
         for list_num in range(len(grid) - 1):
-            if len(grid[list_num]) < 100:
+            if len(grid[list_num]) < 100 and list_num > 0:
                 for uno in range(len(grid[list_num]) - 1):
-                    if grid[list_num][uno] != 0:
-                        if grid[list_num + 1][uno] == 0 and list_num < (len(grid) - 1):
+                    if grid[list_num][uno] != 0 and uno > 0:
+                        if grid[list_num + 1][uno] == 0:
                             perimeter += 1
-                        if grid[list_num][uno + 1] == 0 and uno < (len(grid[list_num]) - 1):
+                        if grid[list_num][uno + 1] == 0:
                             perimeter += 1
-                        if grid[list_num - 1][uno] == 0 and list_num > 0:
+                        if grid[list_num - 1][uno] == 0:
                             perimeter += 1
-                        if grid[list_num][uno - 1] == 0 and uno > 0:
+                        if grid[list_num][uno - 1] == 0:
                             perimeter += 1
 
     return perimeter
