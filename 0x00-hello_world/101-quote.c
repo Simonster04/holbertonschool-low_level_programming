72775af4b27e8314d399b787af9df2e089f98c30
Simonster04 <simon.pahe@gmail.com>
2020-04-25 08:51:07 -0500

    fixing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -8,7 +8,6 @@
 
 int main(void) /*printing with puts*/
 {
-file stdrerr;
-printf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
